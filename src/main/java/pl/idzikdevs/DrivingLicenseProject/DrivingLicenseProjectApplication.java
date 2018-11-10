036583ae79059e3f1e70dd25741839480588c380
Tomasz Idzikowski <idzikdevup@gmail.com>
2018-11-10 07:58:32 +0100

    new version

---
+++ src/main/java/pl/idzikdevs/DrivingLicenseProject/DrivingLicenseProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.idzikdevs.DrivingLicenseProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingLicenseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingLicenseProjectApplication.class, args);
+	}
+}
